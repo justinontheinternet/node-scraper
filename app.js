6dea0c867418d1f78fd91ff444ef3e236f29767a
JUSTIN ARRUDA <justinontheinternet@users.noreply.github.com>
2016-02-16 21:43:45 +0000

    set up modules. trying to access info on page

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 
+var url = 'http://substack.net/images/';
 
+request(url, function(error, response, body){
+  if (!error) {
+    $ = cheerio.load(body);
+    permissions = $('code').text();
+    urls = $('a').attr('href');
+    fileTypes = $('a').match(/\.*/);
+    console.log(fileTypes);
+  } else {
+    console.log("Error:", error);
+  }
+  
+});
+
+
+
+// .pipe(fs.createWriteStream('doodle.png'))
